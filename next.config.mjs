44edc73a59f3b633308f9dfc4c4c656176b27e26
mohak64 <mohak64bansal@gmail.com>
2024-03-15 02:26:50 +0530

    hostname configure

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 // next.config.mjs
 const nextConfig = {
     images: {
-        domains: ['fakestoreapi.com', 'img.freepik.com', 'groww.in', 'leadershipmemphis.org'],
+        domains: ['fakestoreapi.com', 'img.freepik.com', 'groww.in', 'leadershipmemphis.org', 't3.ftcdn.net'],
     },
 };
 
